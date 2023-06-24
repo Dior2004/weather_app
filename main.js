724cc63c8c2f773be159f54172cb85fd8c725cbf
Dior2004 <kupaysinovdiyorbek@gmail.com>
2023-06-24 05:15:44 +0500

    final decision

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-const allSkeleton = document.querySelectorAll(".skeleton");
-window.addEventListener("load", function () {
-  allSkeleton.forEach((item) => item.classList.remove("skeleton"));
-});
-
 const findMyState = () => {
   let address = "";
 
