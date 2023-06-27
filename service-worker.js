20716eba1e2ed2ab31f208eb3912f8d0c21aa86e
Dior2004 <kupaysinovdiyorbek@gmail.com>
2023-06-27 07:09:20 +0500

    serviceworker reinstalled

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("weather-app").then((cache) => {
-      return cache.addAll(["/", "/index.html"]);
+      return cache.addAll(["/", "/index.html", "/style.css", "/main.js"]);
     })
   );
 });
